9f8fa761bee86c6b499c7e2c3e6c7241d6d10cb9
VictorHCBR <victortheveloper@gmail.com>
2023-12-20 23:46:53 -0300

    Working with four verbs.

---
+++ API/Model/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Model;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public Address Address { get; set; } = new Address();
+    public string Gender { get; set; } = string.Empty;
+}
